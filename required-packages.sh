8942169aa84e45e25b562d68e371562e62db0efa
Simon <simon@topwebs.net>
2025-01-09 21:13:17 +0600

    required-packages.sh
    Install required packages
    Install Docker
    Install Docker Compose

---
+++ required-packages.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+# Define color variables
+GREEN="\033[0;32m"     # Green
+YELLOW="\033[1;33m"    # Bright Yellow
+NC="\033[0m"           # No Color
+
+# Display social details and channel information
+echo "==================================="
+echo -e "${YELLOW}		TopWebs		${NC}"
+echo "==================================="
+echo -e "${YELLOW}Telegram: https://t.me/+3eooZdz9J1kwYTBl ${NC}"
+echo -e "${YELLOW}YouTube: https://www.youtube.com/@TopWebsIT ${NC}"
+echo "==================================="
+
+# Update package list
+sudo apt update
+
+# Install required packages if not already installed
+REQUIRED_PACKAGES=(curl jq ca-certificates zlib1g-dev libncurses5-dev libgdbm-dev libnss3-dev git wget make build-essential pkg-config lsb-release libssl-dev libreadline-dev libffi-dev gcc screen unzip lz4)
+
+for package in "${REQUIRED_PACKAGES[@]}"; do
+    if ! dpkg -l | grep -q "^ii  $package "; then
+        echo -e "${YELLOW}Installing $package...${NC}"
+        sudo apt install -y "$package" || { echo "Failed to install $package"; exit 1; }
+    else
+        echo -e "${YELLOW}$package is already installed.${NC}"
+    fi
+done
+
+# Check if Docker is installed
+if ! command -v docker &> /dev/null; then
+    echo -e "${YELLOW}Installing Docker...${NC}"
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+    echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+    sudo apt-get update
+    sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+    docker --version
+else
+    echo -e "${YELLOW}Docker is already installed. Skipping Docker installation.${NC}"
+fi
+
+# Check if Docker Compose is installed
+if ! command -v docker-compose &> /dev/null; then
+    echo -e "${YELLOW}Installing Docker Compose...${NC}"
+    VER=$(curl -s https://api.github.com/repos/docker/compose/releases/latest | grep tag_name | cut -d '"' -f 4)
+    curl -L "https://github.com/docker/compose/releases/download/$VER/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+    sudo chmod +x /usr/local/bin/docker-compose
+    docker-compose --version
+else
+    echo -e "${YELLOW}Docker Compose is already installed. Skipping Docker Compose installation.${NC}"
+fi
+
+# Thank you message
+echo "==================================="
+echo -e "${YELLOW}           TopWebs       ${NC}"
+echo "==================================="
+echo -e "${GREEN}    Thanks for using this script!${NC}"
+echo "==================================="
+echo -e "${YELLOW}Telegram: https://t.me/+3eooZdz9J1kwYTBl ${NC}"
+echo -e "${YELLOW}YouTube: https://www.youtube.com/@TopWebsIT ${NC}"
+echo "======================================================================"
